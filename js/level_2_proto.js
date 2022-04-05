20fa025800426e05f0051108dcf74201f8b3b179
DavideNunin <davide.nunin@gmail.com>
2022-04-05 22:26:29 +0200

    inserita un idea di livello 2 con pulsanti per comprare grano e acqua per gli spaghetti

---
+++ js/level_2_proto.js
@@ -0,0 +1,11 @@
+import {level} from "./util.js";
+
+
+class level_2 extends level{
+    constructor(level){
+        super(level);
+        this.water_price = 0.5;
+        this.grain_price = 1;
+        this
+    }
+}
